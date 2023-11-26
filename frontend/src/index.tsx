685e427f28aee3533450e7e391191e5503b2652c
MiyakeYuuki <research.myky@gmail.com>
2023-11-26 18:42:47 +0900

    :recycle:テストコード名変更

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Chat from './components/Chat';
-import App from './App';
+import TestApp from './TestApp';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
