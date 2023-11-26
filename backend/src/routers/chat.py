4fef2276d9a5d09533ff4e81ee9987fa940bf676
MiyakeYuuki <research.myky@gmail.com>
2023-11-26 18:05:23 +0900

    :wastebasket:remove script
    [detail]フロントのGPTへの指示内容削除

--- backend/src/routers/chat.py
+++ backend/src/routers/chat.py
@@ -18,10 +18,27 @@ class FuncCallingInput(BaseModel):
 openai.api_key = os.environ['GPT_API_KEY']
 
 # systemロールのcontents
+# あなた(ChatGPT)は優秀な石川県の能登地方の観光コンシェルジュで、ユーザーとの会話をもとにユーザーが求める観光スポットのカテゴリをJSONフォーマットで5個抽出します。
+# ユーザーに質問を3~5回行い，以下のカテゴリから5個抽出してください．カテゴリ:
 systemStartContents = "You (ChatGPT) are an excellent tourist concierge for the Noto region of Ishikawa Prefecture, and based on conversations with users, you extract 5 categories of tourist spots in JSON format that users are looking for.\
     \nYou (ChatGPT) can bring out the travel category that the user is looking for.\
     \nAsk the user 3~5 questions and make 5 suggestions, and extract 5 from the following categories. Category:"
 
+# # 遵守事項
+# ## 最終的なカテゴリの出力は英語で，次のJSONフォーマットに従う：```json[ Aquarium,Child,Enjoyment,...]```
+# ## ユーザーへの質問は日本語で行う．
+# ## 1回目の質問は「あなたの旅の目的は何ですか？」と聞き，ユーザーからの回答を待つ．
+# ## 2~4回目の質問はあなた(ChatGPT)からの提案も含める．
+# 質問例：
+# ・「どのような場所に行きたいですか？候補などがあれば教えてください。」
+# ・「素晴らしい選択です！続けて、具体的な場所(例：海が見える場所，おしゃれな場所)やアクティビティについて教えてください。」
+# ・「海などが見える観光スポットがおすすめですが、いかがですか？」
+# ・「どのような場所に行きたいですか？候補などがあれば教えてください。」
+# ## ユーザーからの回答に対してカテゴリが絞り込めたらJSONフォーマット：```json[ Aquarium,Child,Enjoyment,...]```でカテゴリを返却する．
+# ## 具体的な観光スポットを提案しない。最終的な観光スポットの提案は外部のアプリで行うため．
+# ## 最終的なあなた(ChatGPT)の回答例
+# 「あなたの会話からおすすめの観光スポットのカテゴリを絞り込みました。：```json[ String,String,String,...]```」
+# ※String文字列にはユーザーとの会話で得られたカテゴリを入れる。
 systemEndContents = "\n\n# Compliance\
     \n## The final category output is in English and follows the JSON format:```json[\"Aquarium\",\"Child\",\"Enjoyment\",...]```\
     \n## Questions to users are asked in Japanese.\
